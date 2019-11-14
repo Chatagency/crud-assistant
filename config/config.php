13c99b711f0ea148405424c12bad59725ec27f7d
Victor Sanchez <victor@chatagency.com>
2019-11-14 12:10:44 -0400

    Add actions to the config

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 return [
     "actions" => [
         "database" => Chatagency\CrudAssistant\Actions\Database::class,
-        "migration" => \Chatagency\CrudAssistant\Actions\Migration::class,
-        "sanitaion" => \Chatagency\CrudAssistant\Actions\Sanitation::class,
-        "validation" => \Chatagency\CrudAssistant\Actions\Validation::class,
+        "migration" => Chatagency\CrudAssistant\Actions\Migration::class,
+        "sanitaion" => Chatagency\CrudAssistant\Actions\Sanitation::class,
+        "validation" => Chatagency\CrudAssistant\Actions\Validation::class,
     ]
 ];
