437e29f633d8e1d2a4171621f4bc2819aed2071b
Victor Sanchez <victor@chatagency.com>
2019-11-14 12:11:42 -0400

    Work on the Validation action

--- src/Actions/Validation.php
+++ src/Actions/Validation.php
@@ -3,11 +3,48 @@
 namespace Chatagency\CrudAssistant\Actions;
 
 use Chatagency\CrudAssistant\Contracts\ActionInterface;
+use Chatagency\CrudAssistant\DataContainer;
 
 /**
  * Crud Class
  */
 class Validation implements ActionInterface
 {
+    /**
+     * Action type
+     * @var string
+     */
+    protected $type = 'validation';
     
+    /**
+     * Executes action
+     * @param  array $inputs
+     */
+    public function execute(array $inputs){
+        
+        $rules = [];
+        
+        foreach ($inputs as $input) {
+            
+            $name = $input->getName();
+            $fieldValidation = $input
+                    ->getAction($this->type)
+                    ->value
+                ?? null;
+            
+            if($fieldValidation) {
+                if(is_callable($fieldValidation)){
+                    $rules[$name] = $fieldValidation();
+                }
+                else {
+                    $rules[$name] = $fieldValidation;
+                }
+            }
+            else {
+                $rules[$name] = [];
+            }
+        }
+        
+        return $rules;
+    }
 }
--- src/Contracts/ActionInterface.php
+++ src/Contracts/ActionInterface.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace Chatagency\CrudAssistant\Contracts;
+use Chatagency\CrudAssistant\DataContainer;
 
 /**
  * Action Interface
  */
 interface ActionInterface
 {
-    
+    /**
+     * Executes action
+     * @param  array $inputs
+     */
+    public function execute(array $inputs);
 }
