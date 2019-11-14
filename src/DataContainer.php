50d9ebbfff88cc5d4854a6d60063969ab0fcc719
Victor Sanchez <victor@chatagency.com>
2019-11-14 15:31:29 -0400

    Work on DataContainer

--- src/DataContainer.php
+++ src/DataContainer.php
@@ -22,6 +22,12 @@ class DataContainer implements DataContainerInterface
     public $value;
     
     /**
+     * Arbitrary data
+     * @var array
+     */
+    protected $data = [];
+    
+    /**
      * Constructor for data injection
      * @param $key
      * @param $value
@@ -57,6 +63,47 @@ class DataContainer implements DataContainerInterface
         $this->value = $value;
         
         return $this;
+    }
+    
+    public function __get($name)
+    {
+        if(isset($this->data[$name])){
+            return $this->data[$name];
+        }
+        
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined property via __get(): ' . $name .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE
+        );
+        
+        return null;
+    }
+    
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+    
+    public function __isset($name)
+    {
+        return isset($this->data[$value]);
+    }
+    
+    public function __unset($name)
+    {
+        unset($this->data[$name]);
+    }
+    
+    public function __toString()
+    {
+        $data = $this->data;
+        $data['key'] = $this->key;
+        $data['value'] = $this->value;
+        
+        return json_encode($data, JSON_FORCE_OBJECT);
     }
     
 }
