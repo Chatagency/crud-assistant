32101afe5f44539204a2bff71efc8e8772b2bcb7
Victor Sanchez <victor@chatagency.com>
2019-11-15 17:56:28 -0400

    Change generic input to text input

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -3,7 +3,7 @@
 namespace Chatagency\CrudAssistant\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Chatagency\CrudAssistant\Inputs\GenericInput;
+use Chatagency\CrudAssistant\Inputs\TextInput;
 use Chatagency\CrudAssistant\DataContainer;
 
 class InputTest extends TestCase
@@ -12,8 +12,7 @@ class InputTest extends TestCase
     /** @test */
     public function an_input_can_be_created_with_just_a_name()
     {
-        $input = new GenericInput('email');
-        $input->setType('text');
+        $input = new TextInput('email');
         
         $this->assertEquals('email', $input->getName());
         $this->assertEquals('email', $input->getLabel());
@@ -24,7 +23,7 @@ class InputTest extends TestCase
     /** @test */
     public function an_input_can_be_created_with_name_label_and_version()
     {
-        $input = new GenericInput('email', 'Add your email', 2);
+        $input = new TextInput('email', 'Add your email', 2);
         
         $this->assertEquals('email', $input->getName());
         $this->assertEquals('Add your email', $input->getLabel());
@@ -39,7 +38,7 @@ class InputTest extends TestCase
             'email'
         ];
         
-        $input = new GenericInput('email', 'Email', 1);
+        $input = new TextInput('email', 'Email', 1);
         $input->setAction(new DataContainer('validation', $validationValue));
         
         $this->assertEquals($input->getAction('validation')->value, $validationValue);
@@ -49,7 +48,7 @@ class InputTest extends TestCase
     /** @test */
     public function the_label_and_version_can_be_set()
     {
-        $input = new GenericInput('email');
+        $input = new TextInput('email');
         $input->setLabel('Add your email');
         $input->setVersion(2);
         
@@ -61,7 +60,7 @@ class InputTest extends TestCase
     /** @test */
     public function an_arbitrary_attribute_can_be_set()
     {
-        $input = new GenericInput('email');
+        $input = new TextInput('email');
         
         $this->assertNull($input->getAttribute('id'));
         
