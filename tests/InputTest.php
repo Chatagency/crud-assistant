98d8f909c4d75799a6457be1bb1d20878ea88995
Victor Sanchez <victor@chatagency.com>
2019-11-19 14:06:35 -0400

    Change test name

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -50,7 +50,7 @@ class InputTest extends TestCase
     }
     
     /** @test */
-    public function the_label_and_version_can_be_set()
+    public function the_label_and_version_can_be_set_after_the_input_has_been_instantiated()
     {
         $input = new TextInput('email');
         $input->setType('email');
