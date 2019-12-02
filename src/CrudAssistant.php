47fbad855dede2031837827e4acc522163414bea
Victor Sanchez <victor@chatagency.com>
2019-12-02 14:38:13 -0400

    Work on manager's tests

--- src/CrudAssistant.php
+++ src/CrudAssistant.php
@@ -11,26 +11,16 @@ class CrudAssistant
 {
     protected $collections = [];
     
-    public function __constructor()
-    {
-        
-    }
-    
     public static function make()
     {
         return new static();
     }
     
-    public function setCollection($name, array $inputs = [])
+    public function createColletion($name, array $inputs = [])
     {
         $this->collections[$name] = new InputCollection($inputs, new ActionFactory($this->getActionsConfig()));
         
         return $this;
-    }
-    
-    public function getCollection($type)
-    {
-        return  $this->colletion;
     }
     
     public function __get(string $name)
@@ -51,17 +41,8 @@ class CrudAssistant
     
     protected function getActionsConfig()
     {
-        $actionConfig = [];
-        
-        if(function_exists(config())){
-            $actionConfig = config('crud-assistant.actions');
-        }
-        else {
-            $config = require __DIR__.'/../../config/config.php';
-            $actionConfig = $config['actions'];
-        }
-        
-        return $actionConfig;
+        return config('crud-assistant.actions');
     }
+
     
 }
---
+++ tests/CrudAssistantTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Chatagency\CrudAssistant\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Chatagency\CrudAssistant\Inputs\TextInput;
+use Chatagency\CrudAssistant\CrudAssistant;
+use Chatagency\CrudAssistant\Contracts\InputInterface;
+use Chatagency\CrudAssistant\Contracts\InputCollectionInterface;
+use PHPUnit\Framework\Error\Notice;
+
+class CrudAssistantTest extends TestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        $config = require __DIR__.'/../config/config.php';
+        $this->app->config->set('crud-assistant.actions', $config['actions']);
+    }
+    
+    /**
+     * Creates the application.
+     *
+     * Needs to be implemented by subclasses.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = $this->resolveApplication();
+        /**
+         * Just the config resolver is needed
+         */
+        $this->resolveApplicationConfiguration($app);
+        return $app;
+    }
+    
+    /** @test */
+    public function a_collection_can_be_created_using_the_crud_assistant_manager()
+    {
+        $manager = new CrudAssistant();
+        $manager->createColletion('contactForm');
+        
+        $this->assertInstanceOf(InputCollectionInterface::class, $manager->contactForm);
+        
+    }
+    
+    /** @test */
+    public function the_collection_can_be_accessed_directly_using_the_name_assigned_to_it()
+    {
+        $manager = new CrudAssistant();
+        $manager->createColletion('contactForm');
+        $manager->contactForm->add(new TextInput('email'));
+        
+        $this->assertInstanceOf(InputInterface::class, $manager->contactForm->getInput('email'));
+        
+    }
+    
+    /** @test */
+    public function if_a_non_existing_collection_is_accessed_a_php_notice_is_triggered()
+    {
+        $manager = new CrudAssistant();
+        
+        $this->expectException(Notice::class);
+        $manager->contactForm;
+    }
+    
+    /** @test */
+    public function an_crud_assistant_instance_can_be_created_statically()
+    {
+        $manager = CrudAssistant::make();
+        
+        $this->assertInstanceOf(CrudAssistant::class, $manager);
+    }
+    
+}
