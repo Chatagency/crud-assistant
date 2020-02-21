8ae1778116155d7b09a9082244cd564d29e9c523
Victor Sanchez <victor@chatagency.com>
2020-02-21 11:07:29 -0400

    Changes to the Modifiers Factory

--- src/ModifierFactory.php
+++ src/ModifierFactory.php
@@ -14,18 +14,20 @@ class ModifierFactory
     
     public static function make($type)
     {
+        $typeOrig = $type;
+        
         if(!class_exists($type)){
             $type = self::PATH.$type;
         }
         
         if(!class_exists($type)){
-            throw new Exception("The Modifier ".$type. " could not be found or does not exist" , 500);
+            throw new Exception("The Modifier ".$typeOrig. " could not be found or does not exist" , 500);
         }
         
         $modifier = new $type;
         
         if(!$modifier instanceof Modifier) {
-            throw new Exception("The Modifier ".$type. " is not an instance of ". Modifier::class , 500);
+            throw new Exception("The Modifier ".$typeOrig. " is not an instance of ". Modifier::class , 500);
         }
         
         return $modifier;
