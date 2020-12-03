f996d5f12ff4cf9dfdad9320527e64ee378fbfac
Victor Sanchez <victor@chatagency.com>
2020-12-03 11:06:17 -0400

    Fix typos

--- tests/Modifiers/BooleanModifierTest.php
+++ tests/Modifiers/BooleanModifierTest.php
@@ -11,29 +11,29 @@ class BooleanModifierTest extends TestCase
     /** @test */
     public function the_is_boolean_modifier_changes_the_label_of_a_boolean_value()
     {
-        $modifer = new BooleanModifier();
+        $modifier = new BooleanModifier();
         
-        $newTrueValue = $modifer->modify(true, new DataContainer([]));
+        $newTrueValue = $modifier->modify(true, new DataContainer([]));
         $this->assertEquals('Yes', $newTrueValue);
         
-        $newFalseValue = $modifer->modify(false, new DataContainer([]));
+        $newFalseValue = $modifier->modify(false, new DataContainer([]));
         $this->assertEquals('No', $newFalseValue);
     }
     
     /** @test */
     public function the_is_boolean_modifier_labels_can_be_changed_using_the_second_argument()
     {
-        $modifer = new BooleanModifier();
+        $modifier = new BooleanModifier();
         
         $data = new DataContainer([
             'trueLabel' => 'Corrent',
             'falseLabel' => 'Incorrent',
         ]);
         
-        $newTrueValue = $modifer->modify(true, $data);
+        $newTrueValue = $modifier->modify(true, $data);
         $this->assertEquals($data->trueLabel, $newTrueValue);
         
-        $newFalseValue = $modifer->modify(false, $data);
+        $newFalseValue = $modifier->modify(false, $data);
         $this->assertEquals($data->falseLabel, $newFalseValue);
     }
     
